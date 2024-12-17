3a9dbd1e3a293d022cf70adfd08e05854d8cd0fc
gehlotnikhil <111073969+gehlotnikhil@users.noreply.github.com>
2024-12-17 21:32:16 +0530

    setup done

--- server/dist/index.js
+++ server/dist/index.js
@@ -11,21 +11,27 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
-// const {prismaMain} = require("./test")
-// import { PrismaClient } from "@prisma/client";
+const { prismaMain } = require("./test");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 dotenv.config();
 const PORT = process.env.PORT;
 // const prisma = new PrismaClient();
 console.log(PORT);
 app.use(express.json());
-app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    // prismaMain()
-    // .catch((e:any) => {
-    // console.error(e)
-    // })
-    // .finally(async () => await prisma.$disconnect());
+app.get("/", (req, res) => {
+    res.send({ success: true });
+});
+app.get("/test", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const test1 = yield prisma.testing.create({
+        data: {
+            email: "nikhil.doe@example.com",
+            title: "Hello World",
+        },
+    }, { unique: true });
+    console.log("Test created:", test1);
     res.send({ success: true });
 }));
 app.listen(PORT, () => {
-    console.log(`-Server running at port ${PORT}`);
+    console.log(`--> Server running at port ${PORT}`);
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,30 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
-// const {prismaMain} = require("./test")
-// import { PrismaClient } from "@prisma/client";
+const {prismaMain} = require("./test")
+const {PrismaClient} = require("@prisma/client")
+const prisma = new PrismaClient()
 dotenv.config(); 
 const PORT = process.env.PORT as number | unknown;
 
 // const prisma = new PrismaClient();
 console.log(PORT);
 app.use(express.json());
-app.get("/", async(req: any, res: any) => {
-  // prismaMain()
-  // .catch((e:any) => {
-    // console.error(e)
-  // })
-  // .finally(async () => await prisma.$disconnect());
+app.get("/",(req:any,res:any)=>{
+  res.send({success:true})
+})
+app.get("/test", async(req: any, res: any) => {
+  const test1 = await prisma.testing.create({
+    data: {
+        email: "nikhil.doe@example.com",
+        title: "Hello World",
+    },
+  },{unique:true});
+
+  console.log("Test created:", test1)
 
   res.send({ success: true });
 });
 app.listen(PORT, () => {
-  console.log(`-Server running at port ${PORT}`);
+  console.log(`--> Server running at port ${PORT}`);
 });
