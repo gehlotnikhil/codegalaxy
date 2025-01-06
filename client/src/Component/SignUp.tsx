f3295c1b596506899a76ae733b225aca16c18cf4
Nikhil Gehlot <111073969+gehlotnikhil@users.noreply.github.com>
2025-01-06 02:45:13 +0000

    solved:package error

--- client/src/Component/SignUp.tsx
+++ client/src/Component/SignUp.tsx
@@ -1,6 +1,6 @@
 // src/SignUp.tsx
 import React from "react";
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 
 import { useForm, SubmitHandler } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
